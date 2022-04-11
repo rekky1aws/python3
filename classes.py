252eec3badad90166243d8bc5b331ab15e059cac
rekkylaws <donatienhochart2@gmail.com>
2022-04-11 13:50:36 +0200

    Suite de classes.py

--- classes.py
+++ classes.py
@@ -3,6 +3,11 @@ from voiture import Voiture
 v1 = Voiture("Renault", "Modus", "Diesel", "Monospace")
 v2 = Voiture("Ford", "Mustang 68", "Essence", "MuscleCar")
 
-
 print(v1)
 print(v2)
+
+print(v1.get_marque())
+
+v1.set_vitesse(50)
+
+print(v1)
